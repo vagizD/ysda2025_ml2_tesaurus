d148f52b4ab628bf549b93a6acec39a0106369cf
Vagiz Daudov <vagiz.0282@gmail.com>
2025-04-19 16:38:42 +0300

    Check anchor

---
+++ vocab.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def render_md_table(in_path: str, out_path: str):
+    df = pd.read_csv(in_path)
+    # df.fillna("", inplace=True)
+    df.to_markdown(out_path)
+
+
+if __name__ == "__main__":
+    in_path  = "vocab.csv"
+    out_path = "tesaurus.md"
+    render_md_table(in_path, out_path)
